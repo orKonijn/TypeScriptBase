a6cc28471bb9c0dcf07ad22c1950e6dc057c5fec
or <okonijn@gmail.com>
2023-01-11 20:34:13 +0200

    added function 2

--- dist/index.js
+++ dist/index.js
@@ -5,5 +5,16 @@ var getAverage = function (numbers) {
     });
     return sum / numbers.length;
 };
+var getAmountOfPositive = function (numbers) {
+    var sum = 0;
+    numbers.forEach(function (element) {
+        if (element > 0) {
+            sum++;
+        }
+    });
+    return sum;
+};
 var avg = getAverage([0, 100, 2]);
 console.log(avg);
+var getpositive = getAmountOfPositive([1, 3, -4, 3, 2, -5]);
+console.log(getpositive);
--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,16 @@ const getAverage=(numbers: number[]) :number => {
     return sum/numbers.length;
 }
 
+const getAmountOfPositive = (numbers: number[]) :   number =>{
+    let sum =0;
+    numbers.forEach(element => {
+        if(element>0){
+            sum++;
+        }
+    });
+    return sum;
+}
 const avg = getAverage([0,100,2]);
 console.log(avg);
+const getpositive = getAmountOfPositive([1,3,-4,3,2,-5]);
+console.log(getpositive);
