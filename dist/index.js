3ad07272e7f327d75f528d8c29a7fcea488d1d1f
or <okonijn@gmail.com>
2023-01-12 10:21:53 +0200

    added some of part 2

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,21 @@
+"use strict";
+exports.__esModule = true;
+exports.Startf = void 0;
+console.log("here2");
+var fs = require("fs");
+console.log("here3");
+fs.readFile('currencies.txt', 'utf8', function (err, data) {
+    if (err)
+        throw err;
+    var currencies = {};
+    var lines = data.split('\n');
+    for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+        var line = lines_1[_i];
+        var currency = line.split(':');
+        currencies[currency[0].trim()] = Number(currency[1]);
+    }
+    console.log(currencies);
+});
 var getAverage = function (numbers) {
     var sum = 0;
     numbers.forEach(function (element) {
@@ -14,7 +32,84 @@ var getAmountOfPositive = function (numbers) {
     });
     return sum;
 };
-var avg = getAverage([0, 100, 2]);
-console.log(avg);
-var getpositive = getAmountOfPositive([1, 3, -4, 3, 2, -5]);
-console.log(getpositive);
+var sortList = function (numbers) {
+    return numbers.sort();
+};
+var GetFromUser = function () {
+    var Currency = prompt("Enter your Currency");
+    if (typeof Currency == "string") {
+        return Currency;
+    }
+    else {
+        throw "null";
+    }
+};
+var GetFromUserAmount = function () {
+    var num = prompt("sss");
+    var amount;
+    if (typeof num == "string") {
+        amount = +num;
+        return amount;
+    }
+    else {
+        throw "null";
+    }
+};
+var GetCurr = function () {
+    var form = document.querySelector('#myform');
+    if (form != null) {
+        form.onsubmit = function () {
+            var formData = new FormData(form);
+            var text = formData.get('textInput');
+            alert(text);
+            console.log(text);
+            return text;
+        };
+    }
+    throw "null";
+};
+var GetValue = function () {
+};
+var GetAmount = function () {
+};
+var LoopCurrencies = function () {
+    var inputs = "";
+    var num = +inputs;
+    var Currency = "";
+    while (num != -1 && inputs != null) {
+        inputs = prompt("enter currency");
+        if (inputs != null) {
+            num = +inputs;
+        }
+        Currency = prompt("Enter your Currency");
+    }
+};
+var CheckOptions = function (currency, amounts, txts) {
+    switch (txts) {
+        case "get value":
+            var x = GetValue();
+            break;
+        case "get amount":
+            var y = GetAmount();
+            break;
+        case "loop currencies":
+            var z = LoopCurrencies();
+            break;
+    }
+};
+function Startf() {
+    console.log("here1");
+    var x = fs.writeFileSync('C:\Users\OR\Documents\TypeScriptBasic\TypeScriptBase\src\Currencies.txt', 'utf8');
+    console.log(x);
+    var curr = GetFromUser();
+    var amount = GetFromUserAmount();
+    var txt = GetCurr();
+    var checkOption = CheckOptions(curr, amount, txt);
+    console.log(txt);
+}
+exports.Startf = Startf;
+Startf();
+//const avg = getAverage([0,100,2]);
+//console.log(avg);
+//const getpositive = getAmountOfPositive([1,3,-4,3,2,-5]);
+//console.log(getpositive);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
+console.log("here2");
+import * as fs from 'fs';
+console.log("here3");
+fs.readFile('currencies.txt', 'utf8', (err, data) => {
+    if (err) throw err;
 
+    const currencies: {[key: string]: number} = {};
+    const lines = data.split('\n');
 
+    for (const line of lines) {
+        const currency = line.split(':');
+        currencies[currency[0].trim()] = Number(currency[1]);
+    }
+    console.log(currencies);
+});
 
 
 
@@ -11,6 +24,8 @@ const getAverage=(numbers: number[]) :number => {
     return sum/numbers.length;
 }
 
+
+
 const getAmountOfPositive = (numbers: number[]) :   number =>{
     let sum =0;
     numbers.forEach(element => {
@@ -21,6 +36,8 @@ const getAmountOfPositive = (numbers: number[]) :   number =>{
     return sum;
 }
 
+
+
 const sortList=(numbers: number[]) : number[] =>{
     return numbers.sort();
 }
@@ -28,7 +45,112 @@ const sortList=(numbers: number[]) : number[] =>{
 
 
 
-const avg = getAverage([0,100,2]);
-console.log(avg);
-const getpositive = getAmountOfPositive([1,3,-4,3,2,-5]);
-console.log(getpositive);
+
+
+const GetFromUser=():string=>{
+    const Currency : string | null = prompt("Enter your Currency");
+    if(typeof Currency == "string"){
+        return Currency;
+    }
+    else{
+        throw "null";
+    }
+   
+}
+
+const GetFromUserAmount=():number=>{
+    const num : string | null = prompt("sss");
+    let  amount :number;
+    if(typeof num == "string"){
+        amount=+num;
+        return amount;
+    }
+    else{
+        throw "null";
+    }
+    
+}
+
+
+
+
+
+const GetCurr = ():string=>{
+    const form : HTMLFormElement | null = document.querySelector('#myform');
+    if (form != null) {
+    form.onsubmit = () => {
+      const formData = new FormData(form);
+    
+      const text = formData.get('textInput') as string;
+      alert(text);
+      console.log(text);
+      return text;
+    };
+    }
+    throw "null";
+}
+
+
+
+const GetValue =():void=>{
+
+}
+
+
+const GetAmount =():void=>{
+
+}
+
+
+const LoopCurrencies =():void=>{
+    let inputs: string | null  ="";
+    let num : number= +inputs;
+    let Currency : string | null = "";
+    while(num!=-1 && inputs!=null){
+        inputs =prompt("enter currency");
+        if(inputs!= null){
+            num = +inputs;
+        }
+        Currency = prompt("Enter your Currency");
+        
+    }
+
+    
+}
+
+const CheckOptions= (currency:string , amounts:number ,txts:string):void =>{
+    switch(txts){
+        case "get value":
+            const x=GetValue();
+            break;
+        case  "get amount":
+            const y = GetAmount();
+            break;
+        case "loop currencies":
+            const z=LoopCurrencies();
+            break;
+    }
+}
+
+
+
+  export function Startf():any{
+    console.log("here1");
+    const x =fs.writeFileSync('C:\Users\OR\Documents\TypeScriptBasic\TypeScriptBase\src\Currencies.txt','utf8');
+    console.log(x);
+    const curr = GetFromUser();
+    const amount = GetFromUserAmount();
+    const txt =GetCurr();
+    const checkOption = CheckOptions(curr,amount,txt);
+    console.log(txt);
+    
+}
+
+
+
+Startf();
+
+//const avg = getAverage([0,100,2]);
+//console.log(avg);
+//const getpositive = getAmountOfPositive([1,3,-4,3,2,-5]);
+//console.log(getpositive);
